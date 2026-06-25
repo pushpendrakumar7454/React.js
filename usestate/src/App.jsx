37e963e54f142f544fa6f72c2a8add11ec41ba85
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-06-25 22:34:42 +0530

    hooks countet App

--- usestate/src/App.jsx
+++ usestate/src/App.jsx
@@ -43,7 +43,7 @@ const App = () => {
 
         <button
           className="reset"
-          onClick={() => setCount(0)}
+          onClick={() => setcount(0)}
         >
           Reset Counter
         </button>
