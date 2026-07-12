26953cb120564a7356b1ab8b0afcb9e9daaec260
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-07-12 15:54:51 +0530

    api

--- ContaxtApi/src/context/MyContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext,useState } from "react";
-
-export const MyStore=createContext()
-
-export const MyStoreContextProvider=({children})=>{
-   
-
-    return <MyStore.Provider value={{toggle,settoggle,carts,SetCarts}}>{children}</MyStore.Provider>
-}
