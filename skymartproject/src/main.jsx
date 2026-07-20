9e56c38b56105cbd765fcba35a7954214fb87c0b
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-07-20 15:42:29 +0530

    sky mart

--- skymartproject/src/main.jsx
+++ skymartproject/src/main.jsx
@@ -1,17 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router'
-import {MyStoreSkyCartProvider} from './constext/MyContext.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router";
+import { MyStoreSkyCartProvider } from "./constext/MyContext.jsx";
 
-
-createRoot(document.getElementById('root')).render(
-
-    <BrowserRouter>
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <MyStoreSkyCartProvider>
-        <App/>
+      <App />
     </MyStoreSkyCartProvider>
-    </BrowserRouter>
-  
-)
+  </BrowserRouter>,
+);
