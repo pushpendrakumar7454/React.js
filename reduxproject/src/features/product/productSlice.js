9ed7a59e38cc1ea1248781011f0658b57117bbf1
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-07-25 18:27:52 +0530

    redux

--- reduxproject/src/features/product/productSlice.js
+++ reduxproject/src/features/product/productSlice.js
@@ -1,64 +1,8 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
 
-// Fetch All Products
-export const fetchProducts = createAsyncThunk(
-  "product/fetchProducts",
-  async () => {
-    const res = await axios.get("https://dummyjson.com/products");
-    return res.data.products;
-  }
-);
+import axios from 'axios'
 
-// Fetch Single Product
-export const fetchSingleProduct = createAsyncThunk(
-  "product/fetchSingleProduct",
-  async (id) => {
-    const res = await axios.get(`https://dummyjson.com/products/${id}`);
-    return res.data;
-  }
-);
 
-const initialState = {
-  products: [],
-  singleProduct: null,
-  loading: false,
-};
-
-const productSlice = createSlice({
-  name: "product",
-  initialState,
-  reducers: {},
-
-  extraReducers: (builder) => {
-    // All Products
-    builder.addCase(fetchProducts.pending, (state) => {
-      state.loading = true;
-    });
-
-    builder.addCase(fetchProducts.fulfilled, (state, action) => {
-      state.loading = false;
-      state.products = action.payload;
-    });
-
-    builder.addCase(fetchProducts.rejected, (state) => {
-      state.loading = false;
-    });
-
-    // Single Product
-    builder.addCase(fetchSingleProduct.pending, (state) => {
-      state.loading = true;
-    });
-
-    builder.addCase(fetchSingleProduct.fulfilled, (state, action) => {
-      state.loading = false;
-      state.singleProduct = action.payload;
-    });
-
-    builder.addCase(fetchSingleProduct.rejected, (state) => {
-      state.loading = false;
-    });
-  },
-});
-
-export default productSlice.reducer;
+const getData=async()=>{
+    let res= await axios.get("https://dummyjson.com/products")
+    
+}
