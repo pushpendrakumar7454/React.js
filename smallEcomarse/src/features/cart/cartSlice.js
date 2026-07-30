d502fb895bc135f18e37d6bce16620a7ba4f226b
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-07-30 10:42:46 +0530

    smail E-comrase Application

--- smallEcomarse/src/features/cart/cartSlice.js
+++ smallEcomarse/src/features/cart/cartSlice.js
@@ -1,39 +1,50 @@
-import { createSlice } from "@reduxjs/toolkit";
+import {createSlice} from '@reduxjs/toolkit'
+
+
+const saveCart=(cart)=>{
+    localStorage.setItem('cart',JSON.stringify(cart))
+}
+
+
 
 const cartSlice=createSlice({
     name:"cart",
     initialState:{
-        cartItem:[]
+        cartItem:JSON.parse(localStorage.getItem("cart"))||[]
     },
-   reducers:{
-     addtoCart:(state,action)=>{
-        const exist=state.cartItem.find((item)=>item.id==action.payload.id)
-        if(exist){
+    reducers:{
+        addtoCart:(state,action)=>{
+           const exist=state.cartItem.find((item)=>item.id===action.payload.id)
+           if(exist){
             exist.quantity+=1
-        }else{
+           }else{
             state.cartItem.push({...action.payload,quantity:1})
-        }
-    },
-    removeCart:(state,action)=>{
-        state.cartItem=state.cartItem.filter((item)=>item.id!==action.payload)
-    },
-    increment:(state,action)=>{
-        const item=state.cartItem.find((item)=>item.id===action.payload)
-        if(item){
-            item.quantity+=1
-        }
-    },
-    decrement:(state,action)=>{
-        const item=state.cartItem.find((item)=>item.id===action.payload)
-        if(item){
+           }
+           saveCart(state.cartItem)
+        },
+        removeCart:(state,action)=>{
+          state.cartItem= state.cartItem.filter((item)=>item.id!==action.payload)
+          saveCart(state.cartItem)
+        },
+        increment:(state,action)=>{
+            const item=state.cartItem.find((item)=>item.id===action.payload)
+            if(item){
+                item.quantity+=1
+            }
+            saveCart(state.cartItem)
+        },
+        decrement:(state,action)=>{
+           const item=state.cartItem.find((item)=>item.id===action.payload)
+           if(item){
             if(item.quantity>1){
                 item.quantity-=1
             }else{
                 state.cartItem=state.cartItem.filter((item)=>item.id!==action.payload)
             }
+           }
+           saveCart(state.cartItem)
         }
     }
-   }
 })
 
 export const {addtoCart,removeCart,increment,decrement}=cartSlice.actions
