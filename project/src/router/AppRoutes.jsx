df4b3f52d34b36cc2c102fa5dc85a43e8e37571e
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-08-01 23:15:00 +0530

    project

--- project/src/router/AppRoutes.jsx
+++ project/src/router/AppRoutes.jsx
@@ -14,67 +14,66 @@ import CreateStartup from "../pages/CreateStartup";
 import StartupDetails from "../components/StartupDetails";
 import MyStartups from "../components/MyStartups";
 
-
-
 const AppRoutes = () => {
-
   const dispatch = useDispatch();
 
   useEffect(() => {
-
     const loggedInUser =
       JSON.parse(localStorage.getItem("loggedinUser")) || null;
 
     if (loggedInUser) {
       dispatch(addUser(loggedInUser));
     }
-
   }, [dispatch]);
-
 
   const router = createBrowserRouter([
     {
-
       element: <ProtectedRouter />,
-      children: [{
-        path: "/",
-        element: <Layout />,
-        children: [
-          {
-            path: '',
-            element: <Home />
-          }, {
-            path: "/create-startup",
-            element: <CreateStartup />
-          },{
-            path:"productdetail/:id",
-            element:<StartupDetails/>
-            
-          },{
-            path:'my-startups',
-            element:<MyStartups/>
-          },
-        ]
-      }
-      ]
-    }, {
+      children: [
+        {
+          path: "/",
+          element: <Layout />,
+          children: [
+            {
+              path: "",
+              element: <Home />,
+            },
+            {
+              path: "/create-startup",
+              element: <CreateStartup />,
+            },
+            {
+              path: "productdetail/:id",
+              element: <StartupDetails />,
+            },
+            {
+              path: "my-startups",
+              element: <MyStartups />,
+            },
+            {
+              path: "/edit-startup/:id",
+              element: <EditStartup />,
+            },
+          ],
+        },
+      ],
+    },
+    {
       element: <PublicRoute />,
       children: [
         {
-          path: '/login',
-          element: <Login />
-        }, {
-          path: '/register',
-          element: <Register />
-        }
-      ]
-    }
-  ])
-
-
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/register",
+          element: <Register />,
+        },
+      ],
+    },
+  ]);
 
   return <RouterProvider router={router} />;
-
 };
 
 export default AppRoutes;
