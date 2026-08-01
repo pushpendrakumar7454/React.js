d640a87fd7bc3e2978baa66f1f93e341a634be37
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-08-01 17:38:42 +0530

    project

--- 4LayerArchitecture/src/main.jsx
+++ 4LayerArchitecture/src/main.jsx
@@ -1,8 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import { Provider } from 'react-redux'
+import {store} from './app/store.js'
+import AppRoute from './routes/AppRoute.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <Provider store={store}>
+      <AppRoute/>
+    </Provider>
 )
