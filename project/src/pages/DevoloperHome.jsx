ddc1b42d003b89dd3478c87e6a0a81c59a541179
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-08-02 23:06:32 +0530

    project

--- project/src/pages/DevoloperHome.jsx
+++ project/src/pages/DevoloperHome.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import DeveloperHero from '../components/DeveloperHero'
+import DeveloperStats from '../components/DeveloperStats'
 
 const DevoloperHome = () => {
     return (
-        <div>
-            i am developer home
+        <div className='min-h-screen bg-slate-950 p-6'>
+             <DeveloperHero/>
+             <DeveloperStats/>
         </div>
     )
 }
